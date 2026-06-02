d8aa97fa35091c386e4b9aa77a2366eb3fb80984
Madina Aidara <madinaaidara26@gmail.com>
2026-06-02 23:25:02 +0200

    security: hide production api credentials on main branch

--- lib/core/constants/supabase_constants.dart
+++ lib/core/constants/supabase_constants.dart
@@ -1,12 +1,10 @@
 class SupabaseConstants {
 
   // URL de votre projet Supabase
-  static const String supabaseUrl = 'https://qcfynospgeipsaxtvawy.supabase.co';
-  //static const String supabaseUrl = 'VOTRE_URL_SUPABASE_ICI';
+  static const String supabaseUrl = 'VOTRE_URL_SUPABASE_ICI';
 
   // Clé anonyme (public) de votre projet Supabase
-  static const String supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFjZnlub3NwZ2VpcHNheHR2YXd5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc0ODMxOTUsImV4cCI6MjA5MzA1OTE5NX0.Oov4_LbsIK4pZsxpO1R-wRlywdzufgHQq-onszlE5wc';
-  //static const String supabaseAnonKey = 'VOTRE_CLÉ_ANONYME_SUPABASE_ICI';
+  static const String supabaseAnonKey = 'VOTRE_CLÉ_ANONYME_SUPABASE_ICI';
 
   // Noms des tables
   static const String tableProfiles = 'profiles';
